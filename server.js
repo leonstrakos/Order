1e35867b19b5ffff5eb478b010020e80656fb6a7
Leon Strakos <148482464+lstrakosffos@users.noreply.github.com>
2026-03-14 12:57:52 +0100

    test login

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
+      httpOnly: false,
       sameSite: "lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 4
